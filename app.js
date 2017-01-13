17d335c3485faf123ce1c87350cbbd22bc109b9f
Gaye Lowenstein <glowen@hotmail.com>
2017-01-13 10:43:55 -0800

    Init Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+
+
+
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function(){
+  console.log("YelpCamp server has started!");
+});
